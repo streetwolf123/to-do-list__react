73d92fdfb218b5e944a41dc2bfeac0be176c5275
Konrad Tusiński <konrad.tusinski@o2.pl>
2023-05-18 06:51:20 +0200

    changed

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -3,14 +3,14 @@ import "./style.css"
 const Buttons = ({tasks, hideDone, toggleHideDone, toggleAllTasksDone}) => (
     tasks.length > 0 && (
         <div className="section__buttons--flex">
+            <button onClick={toggleHideDone} className="section__button">
+                {hideDone ? "Pokaz" : "Ukryj"} ukończone
+            </button>
             <button 
             onClick={toggleAllTasksDone} 
             className="section__button header__buttonToggleAllDone--flex" disabled={tasks.every(({ done }) => done)}
             >
                 Ukończ wszystkie
-            </button>
-            <button onClick={toggleHideDone} className="section__button">
-                {hideDone ? "Pokaz" : "Ukryj"} ukończone
             </button>
         </div>
     )
